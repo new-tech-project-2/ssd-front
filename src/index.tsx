ca5e1edc2d3a044ee6542263b0aa60eae154de69
bluejoyq <bluejoy32@gmail.com>
2022-10-09 01:35:34 +0900

    fix: eslint bug 수정 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-    RouterProvider,
-} from "react-router-dom";
-import {router} from "./router";
+
 import {RecoilRoot} from "recoil";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <RecoilRoot>
-          <RouterProvider router={router} />
+          <App/>
       </RecoilRoot>
   </React.StrictMode>
 );
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
+import React from 'react';
 
 export const router = createBrowserRouter([
     {
