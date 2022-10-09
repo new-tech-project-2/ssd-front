e9f4429d4bf671dd37081a9ce47ae4ec20324ba9
bluejoyq <bluejoy32@gmail.com>
2022-10-10 01:14:32 +0900

    feat: 데이지 ui를 사용하도록 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [ "cupcake", ],
+  },
+  plugins: [require("@tailwindcss/typography"),require('daisyui')],
 }
