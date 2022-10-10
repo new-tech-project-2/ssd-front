0a9e7eac24c857224cc62d2c81e492ca5c2e50cb
bluejoyq <bluejoy32@gmail.com>
2022-10-10 19:26:31 +0900

    feat: tailwind eslint, prettier 추가

--- src/common/components/textField.tsx
+++ src/common/components/textField.tsx
@@ -1,25 +1,34 @@
 import { ChangeEventHandler } from "react";
-
 export const PrimaryTextField = ({
     label,
     placeholder,
+    isError = true,
+    errorMessage = "",
     onChange,
 }: {
     label: string;
     placeholder: string;
+    isError?: boolean;
+    errorMessage?: string;
     onChange?: ChangeEventHandler<HTMLInputElement>;
 }) => {
     return (
-        <div>
-            <label className="label">
-                <span className="label-text ">{label}</span>
-            </label>
+        <div className="flex flex-col">
             <input
                 onChange={onChange}
                 type="text"
                 placeholder={placeholder}
-                className="input input-bordered input-primary input-lg w-full max-w-xs"
+                className={`input input-bordered ${
+                    isError ? "input-error" : "input-primary"
+                } input-lg w-full max-w-xs`}
             />
+            <label className="">
+                <span
+                    className={`${isError ? "text-error" : "text-transparent"}`}
+                >
+                    {errorMessage}
+                </span>
+            </label>
         </div>
     );
 };
