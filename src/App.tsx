cad6eba166cf5ed01a76b423a68df69041fd77a1
bluejoyq <bluejoy32@gmail.com>
2022-10-17 04:25:29 +0900

    feat: 인증 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { useRecoilValue } from "recoil";
+import authTokenState from "./common/recoil/authTokenAtom";
+import { authenticRouter, needAuthRotuer } from "./routes/router";
 
 const queryClient = new QueryClient();
 function App() {
+    const authToken = useRecoilValue(authTokenState);
+
     return (
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
+            <RouterProvider
+                router={authToken.length > 1 ? authenticRouter : needAuthRotuer}
+            />
         </QueryClientProvider>
     );
 }
