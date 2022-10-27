9c25cc9beeec08e28dd545d6746f7b3c08e4f6c0
bluejoyq <bluejoy32@gmail.com>
2022-10-27 11:35:36 +0900

    fix: recoil bug

--- src/common/recoil/authTokenAtom.ts
+++ src/common/recoil/authTokenAtom.ts
@@ -1,11 +1,7 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
-const { persistAtom } = recoilPersist({ storage: sessionStorage });
 const authTokenState = atom({
     key: "authToken",
     default: "",
-    // eslint-disable-next-line camelcase
-    effects_UNSTABLE: [persistAtom],
 });
 
 export default authTokenState;
