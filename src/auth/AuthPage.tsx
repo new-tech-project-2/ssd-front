3e0a60ec7c1ac55038ab81c1201947f9c2b721c3
Charmull <527wlals@gmail.com>
2022-11-06 23:22:28 +0900

    feat: add logo

--- src/auth/AuthPage.tsx
+++ src/auth/AuthPage.tsx
@@ -37,14 +37,15 @@ const AuthPage = () => {
             }
         >
             <h1 className="pb-10 text-8xl font-black text-primary">SSD</h1>
-            <h2 className="pb-10 text-center text-4xl font-bold text-secondary">
+            {/* <h2 className="pb-10 text-center text-4xl font-bold text-secondary">
                 건전하고 <br />
                 공정한
                 <br />
                 술자리를
                 <br />
                 위하여
-            </h2>
+            </h2> */}
+            <img className="h-48 w-48 pb-10" src="media/logo.svg" />
             <PrimaryTextField
                 label="인증 토큰"
                 placeholder="인증 토큰을 입력하세요"
@@ -65,7 +66,7 @@ const AuthPage = () => {
             >
                 시작하기
             </button>
-            <div className="absolute bottom-5">
+            <div className=" fixed bottom-5">
                 <p className="text-center text-gray-500">
                     &copy;2022 최신 기술 프로젝트 2 TEAM SSD
                 </p>
